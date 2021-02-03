ebf80e831785d6f4c473b71fb5ae614a832667a3
Ginver <74971813+Ginver@users.noreply.github.com>
2021-02-03 15:49:03 +0100

    html en css is gemaakt

---
+++ main.js
@@ -0,0 +1,20 @@
+// const container = document.getElementById('p');
+// console.log(container);
+
+
+
+
+
+// link stylesheet & js
+// create the body in html:
+// create a div
+// 1. create heading: h1 Registreren
+// 2.create form and input fields form input>Gebruikersnaam: + input>Wachtwoord:
+// "Gebruikersnaam mag geen @ bevatten" & "Wachtwoord is te kort, gebruik minimaal 6 tekens"
+
+// add styling in css:
+// 1. mint box
+// 2. white 2 x input field
+// 3. add hover outline turns blue if you stand there with a mouse
+// 4. turn h3 red and let it appear if it's not correctly filled
+// add interaction in js
