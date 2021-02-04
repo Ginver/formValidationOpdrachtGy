d53cab4359b4cc1709ce86f642568b2c9695e63e
Ginver <74971813+Ginver@users.noreply.github.com>
2021-02-04 13:59:51 +0100

    js toegevoegd

--- README.md
+++ README.md
@@ -39,3 +39,19 @@ getoond moet worden.
 // 7. Als dit niet zo is, zorg dan dat de waarschuwing verdwijnt
 // 8. Herhaal deze stappen voor het wachtwoord
 ```
+
+
+GYöngyvér
+// link stylesheet & js
+// create the body in html:
+// create a div
+// 1. create heading: h1 Registreren
+// 2.create form and input fields form input>Gebruikersnaam: + input>Wachtwoord:
+// "Gebruikersnaam mag geen @ bevatten" & "Wachtwoord is te kort, gebruik minimaal 6 tekens"
+
+// add styling in css:
+// 1. mint box
+// 2. white 2 x input field
+// 3. add hover outline turns blue if you stand there with a mouse
+// 4. turn h3 red and let it appear if it's not correctly filled
+// add interaction in js
--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-// const container = document.getElementById('p');
-// console.log(container);
+
+// sla de referenties op naar de inputvelden
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+
+// sla de referenties op naar de warning-messages
+const usernameWarning = document.getElementById('warningUsername');
+const passwordWarning = document.getElementById('warningPassword');
+
+// voeg eventlisteners toe op de inputvelden
+usernameInput.addEventListener('keyup', checkUsername);
+passwordInput.addEventListener('keyup', checkPassWord);
 
 
+// maak een functie die de username input checkt en door de event listener wordt aangeroepen
+function checkUsername(e) {
+    // Wanneer de waarde een @ bevat willen we de melding WEL tonen
+    if (e.target.value.includes('@')) {
+        usernameWarning.textContent = 'Gebruikersnaam mag geen @ bevatten';
+    } else {
+        // in alle andere gevallen tonen we de melding NIET
+        usernameWarning.textContent = '';
+    }
+}
 
-
-
-// link stylesheet & js
-// create the body in html:
-// create a div
-// 1. create heading: h1 Registreren
-// 2.create form and input fields form input>Gebruikersnaam: + input>Wachtwoord:
-// "Gebruikersnaam mag geen @ bevatten" & "Wachtwoord is te kort, gebruik minimaal 6 tekens"
-
-// add styling in css:
-// 1. mint box
-// 2. white 2 x input field
-// 3. add hover outline turns blue if you stand there with a mouse
-// 4. turn h3 red and let it appear if it's not correctly filled
-// add interaction in js
+// maak een functie die de password input checkt en door de event listener wordt aangeroepen
+function checkPassWord (e) {
+    // Als het invoerveld leeg is, of wanneer het wachtwoord lang genoeg is willen we de melding NIET tonen
+    if (e.target.value === '' || e.target.value.length > 6) {
+        passwordWarning.textContent = '';
+    } else {
+        // in alle andere gevallen (e.target.value !== '' && e.target.value.length <= 6) tonen we de melding WEL
+        passwordWarning.textContent = 'Wachtwoord is te kort, gebruik minimaal 6 tekens';
+    }
+}
